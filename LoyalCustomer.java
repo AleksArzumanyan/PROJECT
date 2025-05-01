8c08e62486ab79632c313a140e6169b31e258a66
Aleks <aleks_arzumanyan@edu.aua.am>
2025-05-01 18:38:25 +0400

    LoyalCustomer.java

--- LoyalCustomer.java
+++ LoyalCustomer.java
@@ -1,19 +1,50 @@
+
 /**
- * The LoyalCustomer class extends Customer and tracks loyalty points.
+ * The LoyalCustomer class extends Customer and tracks loyalty points for discount eligibility.
  */
 class LoyalCustomer extends Customer {
     private int loyaltyPoints;
 
-    public LoyalCustomer(String name, String email, String phoneNumber) {
-        super(name, email, phoneNumber);
+    public LoyalCustomer(String name, String surname, String email, String phoneNumber, String passportNumber) {
+        super(name, surname, email, phoneNumber, passportNumber);
         this.loyaltyPoints = 0;
     }
+
 
     public void addPoints(int points) {
         this.loyaltyPoints += points;
     }
 
-    public int getPoints() {
+
+    public int getLoyaltyPoints() {
         return loyaltyPoints;
+    }
+
+
+    public boolean isEligibleForDiscount() {
+        return loyaltyPoints > 0;
+    }
+
+
+    public double calculateFinalPrice(double roomPrice) {
+        double finalPrice = roomPrice;
+
+
+        if (isEligibleForDiscount()) {
+            finalPrice *= 0.9;
+        }
+
+        return finalPrice;
+    }
+
+
+
+    public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass()) return false;
+        LoyalCustomer that = (LoyalCustomer) obj;
+        return getPassportNumber().equals(that.getPassportNumber());
+    }
+    public String toString() {
+        return "GuestID: " + guestID + ", Name: " + name + ", Surname: " + surname + ", Email: " + email + ", Phone: " + phoneNumber + ", Passport: " + passportNumber + ", LoyalityPoints: " + loyaltyPoints;
     }
 }
