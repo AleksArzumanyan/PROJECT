16bcb4641ea6c35da958cdd9858bacf595fe8f67
khachatryanelen <125363710+khachatryanelen@users.noreply.github.com>
2025-05-03 12:43:27 +0400

    Feedback.java

---
+++ Feedback.java
@@ -0,0 +1,33 @@
+/**
+ * The Feedback class represents a feedback entry left by a customer.
+ */
+public class Feedback {
+    private Customer customer;
+    private int rating; // from 1 to 5
+    private String comment;
+
+    public Feedback(Customer customer, int rating, String comment) {
+        this.customer = customer;
+        this.rating = rating;
+        this.comment = comment;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    @Override
+    public String toString() {
+        return "Feedback from " + customer.getName() +
+                " (Phone: " + customer.getPhoneNumber() + ") - " +
+                "Rating: " + rating + "/5, Comment: \"" + comment + "\"";
+    }
+}
