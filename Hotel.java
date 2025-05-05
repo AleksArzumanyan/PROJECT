070ff4261ac9068720605a7d40be0cea9bcaeae2
Aleks <aleks_arzumanyan@edu.aua.am>
2025-05-05 23:48:26 +0400

    Hotel.java

---
+++ Hotel.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Hotel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\nWelcome to the Hotel Booking System!");
+            System.out.println("What would you like to do?");
+            System.out.println("1. Book a Room");
+            System.out.println("2. Book the Gym");
+            System.out.println("3. Book the Restaurant");
+            System.out.println("4. Book an Event Hall");
+            System.out.println("5. Leave Feedback");
+            System.out.println("6. Show Rating Summary");
+            System.out.println("7. Exit");
+
+
+            System.out.print("Enter your choice (1-7): ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    Room.startBookingSystem();
+                    break;
+
+                case 2:
+                    Gym.startGymBooking();
+                    break;
+
+                case 3:
+                    TableReservation.startReservationSystem();
+                    break;
+
+                case 4:
+                    EventReservation.startEventReservationSystem();
+                    break;
+
+                case 5:
+                    Feedback.leaveFeedback(scanner);
+                    break;
+
+                case 6:
+                    Feedback.showRatingSummary();
+                    break;
+
+                case 7:
+                    running = false;
+                    System.out.println("Thank you for using our hotel system!");
+                    break;
+
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+
+        scanner.close();
+    }
+}
